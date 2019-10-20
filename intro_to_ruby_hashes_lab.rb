3d7d04a66507d46beea15a9eacba8919b6304fe0
Skyler Torian <skymanat7@yahoo.com>
2019-10-20 17:56:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,7 @@
 def base_hash
-  monopoly = {:railroads =>
+  monopoly = {:railroads => {
     }
-  
+  }
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 	{}
