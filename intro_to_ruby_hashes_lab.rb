8adf7c2a73c1d849f2a0ad8ce54b685083675b91
Skyler Torian <skymanat7@yahoo.com>
2019-10-20 19:04:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,8 +23,8 @@ def monopoly_with_third_tier
    monopoly = {
   :railroads => {
     :pieces => 4,
-    :rent_in_dollars =>
-    {:one_piece_owned => 25,
+    :rent_in_dollars => {
+      :one_piece_owned => 25,
       :two_pieces_owned => 50,
       :three_pieces_owned => 75,
       :four_pieces_owned => 100
